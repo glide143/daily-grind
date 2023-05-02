16118f230a2fcba2f912cd4d6218565910402cee
melchizedek <melchizedek30@gmail.com>
2023-05-02 20:53:44 +0800

    Separate the class to its packages

--- src/test/java/com/melchizedek/leet/code/grind/easy/SignOfTheProductOfAnArrayTest.java
+++ src/test/java/com/melchizedek/leet/code/grind/easy/SignOfTheProductOfAnArrayTest.java
@@ -1,3 +1,5 @@
+package com.melchizedek.leet.code.grind.easy;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
--- src/test/java/com/melchizedek/leet/code/grind/medium/BulbSwitcherTest.java
+++ src/test/java/com/melchizedek/leet/code/grind/medium/BulbSwitcherTest.java
@@ -1,3 +1,5 @@
+package com.melchizedek.leet.code.grind.medium;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
