b0664610c3ea11c9d082ca5f95ebaa6ca584a889
melchizedek <melchizedek30@gmail.com>
2023-05-02 20:47:29 +0800

    Added 319. Bulb Switcher

---
+++ src/test/java/BulbSwitcherTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BulbSwitcherTest {
+
+    @Test
+    void test_case_1() {
+        int result = bulbSwitch(3);
+        assertEquals(1, result);
+    }
+    @Test
+    void test_case_2() {
+        int result = bulbSwitch(0);
+        assertEquals(0, result);
+    }
+    @Test
+    void test_case_3() {
+        int result = bulbSwitch(1);
+        assertEquals(1, result);
+    }
+
+    private int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
